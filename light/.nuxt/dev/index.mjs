f862d479971450d725f867e56380641144e3568b
huspaasam <huspaas1@gmail.com>
2025-09-13 11:08:14 +0330

    commit 1

--- light/.nuxt/dev/index.mjs
+++ light/.nuxt/dev/index.mjs
@@ -3,40 +3,40 @@ import { Server } from 'node:http';
 import { resolve, dirname, join } from 'node:path';
 import nodeCrypto from 'node:crypto';
 import { parentPort, threadId } from 'node:worker_threads';
-import { defineEventHandler, handleCacheHeaders, splitCookiesString, createEvent, fetchWithEvent, isEvent, eventHandler, setHeaders, sendRedirect, proxyRequest, getRequestHeader, setResponseHeaders, setResponseStatus, send, getRequestHeaders, setResponseHeader, appendResponseHeader, getRequestURL, getResponseHeader, removeResponseHeader, createError, getQuery as getQuery$1, readBody, createApp, createRouter as createRouter$1, toNodeListener, lazyEventHandler, getResponseStatus, getRouterParam, getResponseStatusText } from 'file://G:/Wespaar/light/node_modules/h3/dist/index.mjs';
-import { escapeHtml } from 'file://G:/Wespaar/light/node_modules/@vue/shared/dist/shared.cjs.js';
-import { createRenderer, getRequestDependencies, getPreloadLinks, getPrefetchLinks } from 'file://G:/Wespaar/light/node_modules/vue-bundle-renderer/dist/runtime.mjs';
-import { parseURL, withoutBase, joinURL, getQuery, withQuery, withTrailingSlash, decodePath, withLeadingSlash, withoutTrailingSlash, joinRelativeURL } from 'file://G:/Wespaar/light/node_modules/ufo/dist/index.mjs';
-import { renderToString } from 'file://G:/Wespaar/light/node_modules/vue/server-renderer/index.mjs';
-import { klona } from 'file://G:/Wespaar/light/node_modules/klona/dist/index.mjs';
-import defu, { defuFn } from 'file://G:/Wespaar/light/node_modules/defu/dist/defu.mjs';
-import destr, { destr as destr$1 } from 'file://G:/Wespaar/light/node_modules/destr/dist/index.mjs';
-import { snakeCase } from 'file://G:/Wespaar/light/node_modules/scule/dist/index.mjs';
-import { createHead as createHead$1, propsToString, renderSSRHead } from 'file://G:/Wespaar/light/node_modules/unhead/dist/server.mjs';
-import { stringify, uneval } from 'file://G:/Wespaar/light/node_modules/devalue/index.js';
-import { isVNode, toValue, isRef } from 'file://G:/Wespaar/light/node_modules/vue/index.mjs';
-import { DeprecationsPlugin, PromisesPlugin, TemplateParamsPlugin, AliasSortingPlugin } from 'file://G:/Wespaar/light/node_modules/unhead/dist/plugins.mjs';
-import { createHooks } from 'file://G:/Wespaar/light/node_modules/hookable/dist/index.mjs';
-import { createFetch, Headers as Headers$1 } from 'file://G:/Wespaar/light/node_modules/ofetch/dist/node.mjs';
-import { fetchNodeRequestHandler, callNodeRequestHandler } from 'file://G:/Wespaar/light/node_modules/node-mock-http/dist/index.mjs';
-import { createStorage, prefixStorage } from 'file://G:/Wespaar/light/node_modules/unstorage/dist/index.mjs';
-import unstorage_47drivers_47fs from 'file://G:/Wespaar/light/node_modules/unstorage/drivers/fs.mjs';
-import { digest } from 'file://G:/Wespaar/light/node_modules/ohash/dist/index.mjs';
-import { toRouteMatcher, createRouter } from 'file://G:/Wespaar/light/node_modules/radix3/dist/index.mjs';
+import { defineEventHandler, handleCacheHeaders, splitCookiesString, createEvent, fetchWithEvent, isEvent, eventHandler, setHeaders, sendRedirect, proxyRequest, getRequestHeader, setResponseHeaders, setResponseStatus, send, getRequestHeaders, setResponseHeader, appendResponseHeader, getRequestURL, getResponseHeader, removeResponseHeader, createError, getQuery as getQuery$1, readBody, createApp, createRouter as createRouter$1, toNodeListener, lazyEventHandler, getResponseStatus, getRouterParam, getResponseStatusText } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/h3/dist/index.mjs';
+import { escapeHtml } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/@vue/shared/dist/shared.cjs.js';
+import { createRenderer, getRequestDependencies, getPreloadLinks, getPrefetchLinks } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/vue-bundle-renderer/dist/runtime.mjs';
+import { parseURL, withoutBase, joinURL, getQuery, withQuery, withTrailingSlash, decodePath, withLeadingSlash, withoutTrailingSlash, joinRelativeURL } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/ufo/dist/index.mjs';
+import { renderToString } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/vue/server-renderer/index.mjs';
+import { klona } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/klona/dist/index.mjs';
+import defu, { defuFn } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/defu/dist/defu.mjs';
+import destr, { destr as destr$1 } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/destr/dist/index.mjs';
+import { snakeCase } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/scule/dist/index.mjs';
+import { createHead as createHead$1, propsToString, renderSSRHead } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/unhead/dist/server.mjs';
+import { stringify, uneval } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/devalue/index.js';
+import { isVNode, toValue, isRef } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/vue/index.mjs';
+import { DeprecationsPlugin, PromisesPlugin, TemplateParamsPlugin, AliasSortingPlugin } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/unhead/dist/plugins.mjs';
+import { createHooks } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/hookable/dist/index.mjs';
+import { createFetch, Headers as Headers$1 } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/ofetch/dist/node.mjs';
+import { fetchNodeRequestHandler, callNodeRequestHandler } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/node-mock-http/dist/index.mjs';
+import { createStorage, prefixStorage } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/unstorage/dist/index.mjs';
+import unstorage_47drivers_47fs from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/unstorage/drivers/fs.mjs';
+import { digest } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/ohash/dist/index.mjs';
+import { toRouteMatcher, createRouter } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/radix3/dist/index.mjs';
 import { readFile } from 'node:fs/promises';
-import consola, { consola as consola$1 } from 'file://G:/Wespaar/light/node_modules/consola/dist/index.mjs';
-import { ErrorParser } from 'file://G:/Wespaar/light/node_modules/youch-core/build/index.js';
-import { Youch } from 'file://G:/Wespaar/light/node_modules/nitropack/node_modules/youch/build/index.js';
-import { SourceMapConsumer } from 'file://G:/Wespaar/light/node_modules/source-map/source-map.js';
+import consola, { consola as consola$1 } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/consola/dist/index.mjs';
+import { ErrorParser } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/youch-core/build/index.js';
+import { Youch } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/nitropack/node_modules/youch/build/index.js';
+import { SourceMapConsumer } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/source-map/source-map.js';
 import { AsyncLocalStorage } from 'node:async_hooks';
-import { getContext } from 'file://G:/Wespaar/light/node_modules/unctx/dist/index.mjs';
-import { captureRawStackTrace, parseRawStackTrace } from 'file://G:/Wespaar/light/node_modules/errx/dist/index.js';
+import { getContext } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/unctx/dist/index.mjs';
+import { captureRawStackTrace, parseRawStackTrace } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/errx/dist/index.js';
 import { promises } from 'node:fs';
 import { fileURLToPath } from 'node:url';
-import { dirname as dirname$1, resolve as resolve$1 } from 'file://G:/Wespaar/light/node_modules/pathe/dist/index.mjs';
-import { walkResolver } from 'file://G:/Wespaar/light/node_modules/unhead/dist/utils.mjs';
+import { dirname as dirname$1, resolve as resolve$1 } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/pathe/dist/index.mjs';
+import { walkResolver } from 'file://C:/Users/huspa/Documents/GitHub/Wespaar/light/node_modules/unhead/dist/utils.mjs';
 
-const serverAssets = [{"baseName":"server","dir":"G:/Wespaar/light/server/assets"}];
+const serverAssets = [{"baseName":"server","dir":"C:/Users/huspa/Documents/GitHub/Wespaar/light/server/assets"}];
 
 const assets$1 = createStorage();
 
@@ -48,11 +48,11 @@ const storage = createStorage({});
 
 storage.mount('/assets', assets$1);
 
-storage.mount('root', unstorage_47drivers_47fs({"driver":"fs","readOnly":true,"base":"G:/Wespaar/light","watchOptions":{"ignored":[null]}}));
-storage.mount('src', unstorage_47drivers_47fs({"driver":"fs","readOnly":true,"base":"G:/Wespaar/light/server","watchOptions":{"ignored":[null]}}));
-storage.mount('build', unstorage_47drivers_47fs({"driver":"fs","readOnly":false,"base":"G:/Wespaar/light/.nuxt"}));
-storage.mount('cache', unstorage_47drivers_47fs({"driver":"fs","readOnly":false,"base":"G:/Wespaar/light/.nuxt/cache"}));
-storage.mount('data', unstorage_47drivers_47fs({"driver":"fs","base":"G:/Wespaar/light/.data/kv"}));
+storage.mount('root', unstorage_47drivers_47fs({"driver":"fs","readOnly":true,"base":"C:/Users/huspa/Documents/GitHub/Wespaar/light","watchOptions":{"ignored":[null]}}));
+storage.mount('src', unstorage_47drivers_47fs({"driver":"fs","readOnly":true,"base":"C:/Users/huspa/Documents/GitHub/Wespaar/light/server","watchOptions":{"ignored":[null]}}));
+storage.mount('build', unstorage_47drivers_47fs({"driver":"fs","readOnly":false,"base":"C:/Users/huspa/Documents/GitHub/Wespaar/light/.nuxt"}));
+storage.mount('cache', unstorage_47drivers_47fs({"driver":"fs","readOnly":false,"base":"C:/Users/huspa/Documents/GitHub/Wespaar/light/.nuxt/cache"}));
+storage.mount('data', unstorage_47drivers_47fs({"driver":"fs","base":"C:/Users/huspa/Documents/GitHub/Wespaar/light/.data/kv"}));
 
 function useStorage(base = "") {
   return base ? prefixStorage(storage, base) : storage;
@@ -1012,13 +1012,13 @@ if (!window.__NUXT_DEVTOOLS_TIME_METRIC__) {
 window.__NUXT_DEVTOOLS_TIME_METRIC__.appInit = Date.now()
 `;
 
-const _Bu0u9R9QqebEIRSEmayTFeNaCYHc5HbUlICB7YWjjvg = (function(nitro) {
+const _kZQeogUoIte8iQrbc60cJ0EXcaRMYkWEcku00h18xM = (function(nitro) {
   nitro.hooks.hook("render:html", (htmlContext) => {
     htmlContext.head.push(`<script>${script}<\/script>`);
   });
 });
 
-const rootDir = "G:/Wespaar/light";
+const rootDir = "C:/Users/huspa/Documents/GitHub/Wespaar/light";
 
 const appHead = {"meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":""},{"name":"format-detection","content":"telephone=no"}],"link":[{"rel":"icon","type":"image/x-icon","href":"assets/images/favicon.png"},{"rel":"stylesheet","href":"https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&amp;display=swap"},{"rel":"stylesheet","href":"/assets/css/flaticon.min.css"},{"rel":"stylesheet","href":"/assets/css/fontawesome-5.14.0.min.css"},{"rel":"stylesheet","href":"/assets/css/bootstrap.min.css"},{"rel":"stylesheet","href":"/assets/css/magnific-popup.min.css"},{"rel":"stylesheet","href":"/assets/css/nice-select.min.css"},{"rel":"stylesheet","href":"/assets/css/animate.min.css"},{"rel":"stylesheet","href":"/assets/css/slick.min.css"},{"rel":"stylesheet","href":"/assets/css/style.css"},{"rel":"stylesheet","href":"/assets/css/custom.css"}],"style":[],"script":[],"noscript":[],"title":"Ygency - Web Agency Vue NuxtJS Template","htmlAttrs":{"lang":"en"}};
 
@@ -1037,7 +1037,7 @@ const devReducers = {
   URL: (data) => data instanceof URL ? data.toString() : void 0
 };
 const asyncContext = getContext("nuxt-dev", { asyncContext: true, AsyncLocalStorage });
-const _s8GeiGcYx9UE2m6u47xipNXcOT69UVSskwOlukNGOdY = (nitroApp) => {
+const _58oYnm_NvXoYPocYhwxfSmKQ0r2m7V2_KOz8RVxlK9M = (nitroApp) => {
   const handler = nitroApp.h3App.handler;
   nitroApp.h3App.handler = (event) => {
     return asyncContext.callAsync({ logs: [], event }, () => handler(event));
@@ -1107,8 +1107,8 @@ function onConsoleLog(callback) {
 }
 
 const plugins = [
-  _Bu0u9R9QqebEIRSEmayTFeNaCYHc5HbUlICB7YWjjvg,
-_s8GeiGcYx9UE2m6u47xipNXcOT69UVSskwOlukNGOdY
+  _kZQeogUoIte8iQrbc60cJ0EXcaRMYkWEcku00h18xM,
+_58oYnm_NvXoYPocYhwxfSmKQ0r2m7V2_KOz8RVxlK9M
 ];
 
 const assets = {};
@@ -1136,7 +1136,7 @@ function getAsset (id) {
 
 const METHODS = /* @__PURE__ */ new Set(["HEAD", "GET"]);
 const EncodingMap = { gzip: ".gz", br: ".br" };
-const _NTkzC5 = eventHandler((event) => {
+const _XACOm7 = eventHandler((event) => {
   if (event.method && !METHODS.has(event.method)) {
     return;
   }
@@ -1272,7 +1272,7 @@ function publicAssetsURL(...path) {
 const APP_ROOT_OPEN_TAG = `<${appRootTag}${propsToString(appRootAttrs)}>`;
 const APP_ROOT_CLOSE_TAG = `</${appRootTag}>`;
 const getServerEntry = () => Promise.resolve().then(function () { return server$1; }).then((r) => r.default || r);
-const getClientManifest = () => import('file://G:/Wespaar/light/.nuxt//dist/server/client.manifest.mjs').then((r) => r.default || r).then((r) => typeof r === "function" ? r() : r);
+const getClientManifest = () => import('file://C:/Users/huspa/Documents/GitHub/Wespaar/light/.nuxt//dist/server/client.manifest.mjs').then((r) => r.default || r).then((r) => typeof r === "function" ? r() : r);
 const getSSRRenderer = lazyCachedFunction(async () => {
   const manifest = await getClientManifest();
   if (!manifest) {
@@ -1518,13 +1518,13 @@ async function getIslandContext(event) {
   return ctx;
 }
 
-const _lazy_1eYqwN = () => Promise.resolve().then(function () { return renderer$1; });
+const _lazy_GES9A1 = () => Promise.resolve().then(function () { return renderer$1; });
 
 const handlers = [
-  { route: '', handler: _NTkzC5, lazy: false, middleware: true, method: undefined },
-  { route: '/__nuxt_error', handler: _lazy_1eYqwN, lazy: true, middleware: false, method: undefined },
+  { route: '', handler: _XACOm7, lazy: false, middleware: true, method: undefined },
+  { route: '/__nuxt_error', handler: _lazy_GES9A1, lazy: true, middleware: false, method: undefined },
   { route: '/__nuxt_island/**', handler: _SxA8c9, lazy: false, middleware: false, method: undefined },
-  { route: '/**', handler: _lazy_1eYqwN, lazy: true, middleware: false, method: undefined }
+  { route: '/**', handler: _lazy_GES9A1, lazy: true, middleware: false, method: undefined }
 ];
 
 function createNitroApp() {
